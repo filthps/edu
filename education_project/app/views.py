75b5a4ff2baf47613a0a01c38ab21b66e5329b44
Виктор Литовченко <filpsixi@mail.ru>
2024-03-01 23:36:56 +0300

    todo

--- education_project/app/views.py
+++ education_project/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.db.models import F, Q, Sum, Func
+from models import *
 
-# Create your views here.
+#  todo
+free_groups = Education.objects.prefetch_related("course").filter(maxlisteners__gte=Func(F("id"), function="COUNT"))
